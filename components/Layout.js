25355b52404ecf18dabe73e2fb893de44c266c0f
meloalves <meloalvesjuliana@gmail.com>
2023-08-09 18:24:06 -0300

    Enviando projeto

---
+++ components/Layout.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <title>PokeNext</title>
+      </Head>
+      <Navbar />
+        <main className="main-container">
+          {children}
+        </main>
+      <Footer />
+    </>
+  )
+}
